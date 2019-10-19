671cb970a063b42fe7f7a57c37a2a7dee046dfbc
Aielen <smoronskisebastian@gmail.com>
2019-10-19 19:32:06 +0200

    Front

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,7 +3,6 @@
 
 namespace PI\Controller;
 
-use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +14,8 @@ class IndexController extends AbstractController
      * @return Response
      * @Route("/", name="index")
      */
-    public function index(Connection $connection)
+    public function index()
     {
-        dump($connection->executeQuery("SELECT * FROM test")->fetchAll());
-        die();
-
         return $this->render("/index/index_page.html.twig");
     }
 
