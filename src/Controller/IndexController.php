3a748acb4b2e4f8e0791a3b6f168105bdbdc7b5e
Aielen <smoronskisebastian@gmail.com>
2019-10-19 19:37:52 +0200

    Route: /v

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -19,4 +19,13 @@ class IndexController extends AbstractController
         return $this->render("/index/index_page.html.twig");
     }
 
+    /**
+     * @Route("/v", name="current_version")
+     */
+    public function currentVersion()
+    {
+        $version = shell_exec("git rev-parse --verify HEAD");
+        return new Response($version);
+    }
+
 }
